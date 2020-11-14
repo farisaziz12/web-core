ae2758693715d01b1bcde3050ff8df1a7a219249
farisaziz12 <farisaziz12@gmail.com>
2020-11-14 23:17:56 +0100

    feat(auth): export auth class

---
+++ packages/auth/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Auth = void 0;
+var auth_1 = require("./auth");
+Object.defineProperty(exports, "Auth", { enumerable: true, get: function () { return auth_1.Auth; } });
