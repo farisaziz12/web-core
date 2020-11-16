6a63f04bdc86382157406c2bc9b02701c36573da
Faris Aziz <farisaziz12@gmail.com>
2020-11-16 16:29:06 +0100

    chore(auth-tests): add auth class checks and sign up method tests

--- packages/auth/tests/auth.test.js
+++ packages/auth/tests/auth.test.js
@@ -22,6 +22,25 @@ describe("Auth Class", () => {
         }
     });
 
+    // Mocking signUp function to return promise with user object if passwords match
+    Auth.prototype.signUp = jest.fn((email, password, passwordConfirm) => {
+        if (email && password === passwordConfirm) {
+            return Promise.resolve({ email, password });
+        } else {
+            throw new Error("Passwords do not match");
+        }
+    });
+
+    test("Auth class exists", () => {
+        expect(Auth).toBeDefined();
+    });
+
+    test("Auth class instance can be initialized with config", () => {
+        const auth = new Auth(mockConfig);
+
+        expect(auth).toBeDefined();
+    });
+
     test("Auth class instance login method returns user object with correct login arguments", async () => {
         const auth = new Auth(mockConfig);
         const result = await auth.login("user@test.com", "userPassword");
@@ -34,6 +53,33 @@ describe("Auth Class", () => {
 
         expect(() => {
             auth.login();
+        }).toThrow();
+    });
+
+    test("Auth class instance signUp method returns user object with correct signUp arguments", async () => {
+        const auth = new Auth(mockConfig);
+        const result = await auth.signUp(
+            "user@test.com",
+            "userPassword",
+            "userPassword"
+        );
+
+        expect(result.email).toBe(expectedResult.email);
+    });
+
+    test("Auth class instance signUp method throws error when passwords don't match", () => {
+        const auth = new Auth(mockConfig);
+
+        expect(() => {
+            auth.signUp("user@test.com", "userPassword", "usrPassword");
+        }).toThrow();
+    });
+
+    test("Auth class instance signUp method throws error when no password confirmation is given", () => {
+        const auth = new Auth(mockConfig);
+
+        expect(() => {
+            auth.signUp("user@test.com", "userPassword");
         }).toThrow();
     });
 });
