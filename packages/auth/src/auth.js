ce5f0af0671ede9a07128f344645588d89d7c70d
Faris Aziz <53216647+farisaziz12@users.noreply.github.com>
2021-02-27 16:40:17 +0100

    chore() update ts config

--- packages/auth/src/auth.js
+++ packages/auth/src/auth.js
@@ -4,71 +4,66 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Auth = void 0;
-var app_1 = __importDefault(require("firebase/app"));
+const app_1 = __importDefault(require("firebase/app"));
 require("firebase/auth");
-var Auth = /** @class */ (function () {
-    function Auth(config) {
+class Auth {
+    constructor(config) {
         this.app = app_1.default.initializeApp(config);
         this.auth = app_1.default.auth();
     }
-    Auth.prototype.login = function (email, password) {
-        var _this = this;
-        return new Promise(function (resolve, reject) {
-            _this.auth
+    login(email, password) {
+        return new Promise((resolve, reject) => {
+            this.auth
                 .signInWithEmailAndPassword(email, password)
-                .then(function () { return resolve(_this.auth.currentUser); })
-                .catch(function (error) { return reject(error.message); });
+                .then(() => resolve(this.auth.currentUser))
+                .catch((error) => reject(error.message));
         });
-    };
-    Auth.prototype.googleLogin = function () {
-        var _this = this;
-        var provider = new app_1.default.auth.GoogleAuthProvider();
+    }
+    googleLogin() {
+        const provider = new app_1.default.auth.GoogleAuthProvider();
         provider.addScope("profile");
         provider.addScope("email");
-        return new Promise(function (resolve, reject) {
-            _this.auth.signInWithPopup(provider)
-                .then(function (resp) { return resolve(resp.user); })
-                .catch(function (error) { return reject(error.message); });
+        return new Promise((resolve, reject) => {
+            this.auth.signInWithPopup(provider)
+                .then((resp) => resolve(resp.user))
+                .catch((error) => reject(error.message));
         });
-    };
-    Auth.prototype.signUp = function (email, password, passwordConfirm) {
-        var _this = this;
+    }
+    signUp(email, password, passwordConfirm) {
         if (email && password === passwordConfirm) {
-            return new Promise(function (resolve, reject) {
-                _this.auth
+            return new Promise((resolve, reject) => {
+                this.auth
                     .createUserWithEmailAndPassword(email, password)
-                    .then(function () {
+                    .then(() => {
                     /*
                     Sends verification email using firebase
                     if sign up is successful
                     */
-                    _this.auth.currentUser.sendEmailVerification();
-                    resolve(_this.auth.currentUser);
+                    this.auth.currentUser.sendEmailVerification();
+                    resolve(this.auth.currentUser);
                 })
-                    .catch(function (error) { return reject(error.message); });
+                    .catch((error) => reject(error.message));
             });
         }
         else {
             throw new Error("Passwords do not match");
         }
-    };
-    Auth.prototype.signOut = function () {
-        var _this = this;
-        return new Promise(function (resolve, reject) {
-            _this.auth.signOut()
-                .then(function () {
+    }
+    signOut() {
+        return new Promise((resolve, reject) => {
+            this.auth.signOut()
+                .then(() => {
                 // Sign-out successful
                 resolve({
                     success: true
                 });
             })
-                .catch(function (error) { return reject(error.message); });
+                .catch((error) => reject(error.message));
         });
-    };
-    Auth.prototype.getCurrentUser = function () {
-        var _this = this;
-        return new Promise(function (resolve, reject) {
-            _this.auth.onAuthStateChanged(function (currentUser) {
+    }
+    getCurrentUser() {
+        return new Promise((resolve, reject) => {
+            this.auth.onAuthStateChanged((currentUser) => {
                 if (currentUser) {
                     //  If user is logged in firebase returns current user data
                     resolve(currentUser);
@@ -79,7 +74,6 @@ var Auth = /** @class */ (function () {
                 }
             });
         });
-    };
-    return Auth;
-}());
+    }
+}
 exports.Auth = Auth;
