2a9defd70f62f36f243e78995429e9de1afb7063
Faris Aziz <53216647+farisaziz12@users.noreply.github.com>
2021-02-28 19:50:39 +0100

    refactor(components-button): turn into class

--- packages/components/src/Buttons/main.js
+++ packages/components/src/Buttons/main.js
@@ -1,39 +1,96 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styles from './main.module.css'
 
-export function Button({
-  children,
-  text,
-  hex: backgroundColor,
-  className,
-  onClick,
-  disabled,
-  size,
-  style
-}) {
-  const styleObj = {
-    ...style,
-    backgroundColor
+export class Button extends Component {
+  constructor(props) {
+    super(props)
+    const {
+      children,
+      text,
+      hex,
+      className,
+      onClick,
+      disabled,
+      size,
+      style,
+      center,
+      left,
+      right,
+      border,
+      type
+    } = props
+
+    const borderColor = hex && border ? `solid 3px ${hex}` : ''
+    const backgroundColor = hex && !border ? hex : '#808080'
+    const color = hex && border ? hex : '#FFFFFF'
+
+    this.styleObj = {
+      ...style,
+      backgroundColor,
+      color,
+      border: borderColor
+    }
+
+    this.children = children
+    this.text = text
+    this.className = className
+    this.onClickFunc = onClick
+    this.disabled = disabled
+    this.size = size
+    this.type = type
+    this.center = center
+    this.right = right
+    this.left = left
+
+    this.renderButton = this.renderButton.bind(this)
   }
 
-  const getStyles = () => {
-    if (styles[size]) {
-      return styles[size]
-    } else {
-      return styles.default
+  onClick(e) {
+    if (this.onClickFunc) {
+      this.onClickFunc(e)
     }
   }
 
-  const classes = `${className} ${getStyles()} ${disabled && styles.disabled}`
+  getClasses = () => {
+    const classes = []
+    if (this.className) {
+      classes.push(this.className)
+    }
+    if (styles[this.size]) {
+      classes.push(styles[this.size])
+    } else {
+      classes.push(styles.default)
+    }
+    if (this.disabled) {
+      classes.push(styles.disabled)
+    }
+    if (this.center) {
+      classes.push(styles.center)
+    }
+    if (this.right) {
+      classes.push(styles.right)
+    }
+    if (this.left) {
+      classes.push(styles.left)
+    }
+    return classes.join(' ')
+  }
 
-  return (
-    <button
-      disabled={disabled}
-      className={classes}
-      style={styleObj}
-      onClick={(e) => onClick(e)}
-    >
-      {children || text}
-    </button>
-  )
+  renderButton() {
+    return (
+      <button
+        disabled={this.disabled}
+        className={this.getClasses()}
+        style={this.styleObj}
+        onClick={(e) => this.onClick(e)}
+        type={this.type}
+      >
+        {this.text || this.children}
+      </button>
+    )
+  }
+
+  render() {
+    return this.renderButton()
+  }
 }
