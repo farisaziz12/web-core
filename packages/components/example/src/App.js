beda3104955feaef2c3d3611da8bb551a9b40c42
Faris Aziz <53216647+farisaziz12@users.noreply.github.com>
2021-02-28 19:49:56 +0100

    feat(components-example): remove component use

--- packages/components/example/src/App.js
+++ packages/components/example/src/App.js
@@ -1,21 +1,10 @@
 import React from 'react'
 
-import { Button } from 'components'
+import { Button, ShowButton } from 'components'
 import 'components/dist/index.css'
 
 const App = () => {
-  return (
-    <Button
-      className='hello'
-      hex='#ff0000'
-      text='test'
-      onClick={(e) => console.log(e)}
-      disabled
-      size='large'
-    >
-      Hello
-    </Button>
-  )
+  return <></>
 }
 
 export default App
